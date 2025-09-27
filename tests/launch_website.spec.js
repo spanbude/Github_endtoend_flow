187d349ef57332b231474baa3f8ade099503ad27
swapnil <spanbude17@gmail.com>
2025-09-27 20:09:13 +0530

    launch_website_TC002

--- tests/launch_website.spec.js
+++ tests/launch_website.spec.js
@@ -11,3 +11,17 @@ await page.locator("//button[@type='submit']").click();
 
 
 });
+
+test('TC001',async({page})=>{
+
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+await page.locator("//input[@placeholder='Username']").fill("Admin")
+await page.locator("//input[@placeholder='Password']").fill("admin123")
+await page.locator("//button[@type='submit']").click();
+const ttitleofpage=await page.title()
+console.log(ttitleofpage);
+
+
+
+
+});
